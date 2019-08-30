196465041c64da52e4212e3880b7d81a9a31955e
Norberta Balaisyte <nbalaisyte@spartaglobal.com>
2019-08-30 08:46:36 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lab1 homework = new lab1();
+            int summation = 2;
+            int value = homework.multipl(1, 2, 3, out summation);
+            Console.WriteLine(summation);
+            Console.WriteLine(value);
+            Console.Read();
+
+        }
+    }
+    class lab1
+    {
+        public int multipl(int firstnumb, int secondnumb, int thirdnumb, out int summation)
+        {
+            summation = firstnumb + secondnumb+ thirdnumb;
+            return (firstnumb * secondnumb * thirdnumb);
+
+        }
+    }
+}
